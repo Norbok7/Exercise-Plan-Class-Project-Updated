b3eef19c1105b3148146dfc65a4cdd07ef2215d7
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-11-20 11:53:47 -0600

    fixing task it names, reinstalling node

--- src/app/Auth/auth.component.ts
+++ src/app/Auth/auth.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css'],
+  
 })
 export class AuthComponent {
   // * Properties
